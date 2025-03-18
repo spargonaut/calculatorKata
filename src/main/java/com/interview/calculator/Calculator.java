cf3a346a7218a7712937dda61e2c47a1196dc4a1
spargonaut <chiapet@spargonaut.com>
2025-03-18 12:30:10 -0700

    Return 0 when given null;

--- src/main/java/com/interview/calculator/Calculator.java
+++ src/main/java/com/interview/calculator/Calculator.java
@@ -1,4 +1,7 @@
 package com.interview.calculator;
 
 public class Calculator {
+    public String add(String values) {
+        return "0";
+    }
 }
--- src/test/java/com/interview/calculator/CalculatorTest.java
+++ src/test/java/com/interview/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package com.interview.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CalculatorTest {
@@ -11,5 +12,15 @@ public class CalculatorTest {
     public void shouldCreateACalculatorInstance() {
         Calculator calculator = new Calculator();
         assertNotNull(calculator);
+    }
+
+    @Test
+    public void shouldProduceZeroWhenGivenNull() {
+        Calculator calculator = new Calculator();
+
+        String actualResult = calculator.add(null);
+
+        String expectedResult = "0";
+        assertEquals(expectedResult, actualResult);
     }
 }
